6acf40e767d76129122dc6e1755c3e4fa2cc44cf
Kristen <kgrace61@gmail.com>
2024-02-05 20:57:10 -0600

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+function writeCards(names, eventName) {
+    const messages=[];
+    for (let i=0; i<names.length; i++){
+        const message =(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+        messages.push(message)
+        debugger;
+    }
+        
+    
+    return messages;}
+    const names=["Charlie", "Samip", "Ali"];
+    const eventName="birthday";
+    const result= writeCards(["Charlie", "Samip", "Ali"], "birthday");
+    console.log(result)
+
+
+    function countDown(startingNumber) {
+        let countdown =10;
+        while (countdown >=0){
+            console.log(countdown--);
+        }
+
+    }
