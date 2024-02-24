1fe25d56b65604f174c2fdd5e3b63290cae2b961
Kristen <kgrace61@gmail.com>
2024-02-24 16:52:21 -0600

    updated lab

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
+function writeCards(namesArray, eventName) {
+    let messagesArray = []
+    for (let i=0; i <namesArray.length; i++){
+    let message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+    messagesArray.push(message);
+   
+    debugger;
+}
+return messagesArray;
+}
+writeCards((["Guadalupe", "Ollie", "Aki"]), birthday)
 
-function writeCards(names, eventName) {
-    const messages=[];
-    for (let i=0; i<names.length; i++){
-        const message =(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
-        messages.push(message)
-        debugger;
+function countDown() {
+    let countdown = 10;
+    while (countdown >=0){
+        console.log(countdown--)
     }
-        
-    
-    return messages;}
-    const names=["Charlie", "Samip", "Ali"];
-    const eventName="birthday";
-    const result= writeCards(["Charlie", "Samip", "Ali"], "birthday");
-    console.log(result)
-
-
-    function countDown(startingNumber) {
-        let countdown =10;
-        while (countdown >=0){
-            console.log(countdown--);
-        }
-
-    }
+}
+countDown(10);
